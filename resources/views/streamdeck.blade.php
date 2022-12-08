a571ae62eb9ffe409ccc7606eafa820aa72e37dd
Deto <berta.attila.21@gmail.com>
2022-12-08 14:15:28 +0100

    Create streamdeck.blade.php
    Create and add Default DIV-s to site.

---
+++ resources/views/streamdeck.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BTwinsT - Streamdeck</title>
+</head>
+<body>
+    <div class="top small-tabs">
+
+    </div>
+    <div class="upper tabs">
+
+    </div>
+    <div class="lower tabs">
+
+    </div>
+    <div class="bottom small-tabs">
+
+    </div>
+</body>
+</html>
