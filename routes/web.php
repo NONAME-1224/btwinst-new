d3589b222969c93bce23c9e18c2b358408f87a0c
Erix <62739056+NONAME-1224@users.noreply.github.com>
2022-12-08 14:13:28 +0100

    Create StreamDeck model
    "/streamdeck" endpoint now show the streamdeck.blade.php file.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/streamdeck', [\App\Http\Controllers\StreamDeckController::class, "index"]);
